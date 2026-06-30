994a3e870523fad8427442b6fad29d3b3d0a1d60
JorgenMJobloop <jorgen@jobloop.no>
2026-06-30 13:17:56 +0200

    added a small comment

--- Webshop.Api/Models/Product.cs
+++ Webshop.Api/Models/Product.cs
@@ -3,5 +3,5 @@ public class Product
     public Guid Id { get; init; }
     public string Name { get; init; } = string.Empty;
     public double Price { get; init; }
-    public int StockQuantity { get; set; }
+    public int StockQuantity { get; set; } // should preferably be "init", however, this scoping creates undesired behaviour in the reposity layer, the service layer and the controller.
 }
